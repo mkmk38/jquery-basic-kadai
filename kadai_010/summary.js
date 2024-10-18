5eaf0d10305757b9c080a0a38ec1c15f1c4ceb79
mkmk38 <mtnb03@gmail.com>
2024-10-18 21:47:33 +0900

    「kadai_010」フォルダをアップロード

---
+++ kadai_010/summary.js
@@ -0,0 +1,21 @@
+$(function () {
+  $("#change-color").on({
+    click: () => {
+      $("#target").css("color", "red");
+    },
+  });
+
+  $("#change-text").on({
+    click: () => {
+      $("#target").text("こんばんは！");
+    },
+  });
+
+  $("#fade-out").on("click", function () {
+    $("#target").fadeOut();
+  });
+
+  $("#fade-in").on("click", function () {
+    $("#target").fadeIn();
+  });
+});
